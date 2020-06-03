e084702eee863759ed021086c5f3002f3343df2c
stefian <stefan.ianta@w3ai.net>
2020-06-03 08:22:29 -0300

    added PostCreate component and included in App.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import PostCreate from './PostCreate';
 
 export default () => {
-    return <div>Blog app</div>
+    return (<div>
+        <h1>Create Post</h1>
+        <PostCreate />
+    </div>
+    );
 };
