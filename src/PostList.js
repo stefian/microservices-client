394f7d14c83f7549c391e55e473c67ff07380188
stefian <stefan.ianta@w3ai.net>
2020-06-03 13:59:19 -0300

    added rendering of post cards on the page

--- src/PostList.js
+++ src/PostList.js
@@ -16,7 +16,20 @@ export default () => {
     fetchPosts();
   }, []); // empty array tells react to run the function just one time
 
-  console.log(posts);
+  // for each post in the array of posts
+  const renderedPosts = Object.values(posts).map(post => {
+    return <div
+      className="card"
+      style={{ width: '30%', marginBottom: '20px' }}
+      key={post.id}
+    >
+      <div className="card-body">
+        <h3>{post.title}</h3>
+      </div>
+    </div>
+  });
 
-  return <div />;
+  return <div className="d-flex flex-row flex-wrap justify-content-between" >
+    {renderedPosts}
+  </div>;
 };
