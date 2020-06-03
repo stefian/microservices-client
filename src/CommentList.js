78cbaa64c513900b25109da1fb3ed4ce0d26de6e
stefian <stefan.ianta@w3ai.net>
2020-06-03 15:18:43 -0300

    added the fetch comments function

--- src/CommentList.js
+++ src/CommentList.js
@@ -1,6 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export default () => {
+export default ({ postId }) => {
+  const [comments, setComments] = useState([]);
+
+  const fecthData = async () => {
+    const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
+
+    setComments(res.data);
+  };
+
+  useEffect(() => {
+    fecthData();
+  }, []);
+
   return <div />;
 };
