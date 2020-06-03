4ce6a8f5e54eb6eacf3f28f1d29cafd4f2244b6f
stefian <stefan.ianta@w3ai.net>
2020-06-03 13:03:43 -0300

    added posts state, fetchPosts and useEffect for first rendering

--- src/PostList.js
+++ src/PostList.js
@@ -1,5 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react'; // useEffect - to be used when comp created
+import axios from 'axios';
+
 
 export default () => {
+  const [posts, setPosts] = useState({});
+
+  const fetchPosts = async () => {
+    const res = await axios.get('http://localhost:4000/posts');
+
+    setPosts(res.data);
+  };
+
+  // call fetchPosts when PostList first rendered 
+  useEffect(() => {
+    fetchPosts();
+  }, []); // empty array tells react to run the function just one time
+
+  console.log(posts);
+
   return <div />;
 };
