f546efc0752e87990ef50fe2d1a3f8619507879b
stefian <stefan.ianta@w3ai.net>
2020-06-03 08:51:09 -0300

    added onSubmit + axios request to Posts microservice / 4000

--- src/PostCreate.js
+++ src/PostCreate.js
@@ -3,8 +3,19 @@ import axios from 'axios';
 
 export default () => {
 	const [title, setTitle] = useState('');
+
+	const onSubmit = async (event) => {
+		event.preventDefault();
+
+		await axios.post('http://localhost:4000/posts', {
+			title
+		});		// set request body to title
+
+		setTitle('');
+	};
+
 	return (<div>
-		<form>
+		<form onSubmit={onSubmit} >
 			<div className="form-group">
 				<label>Title</label>
 				<input value={title} onChange={e => setTitle(e.target.value)} className="form-control" />
