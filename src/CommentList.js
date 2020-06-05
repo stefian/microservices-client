aa2336408fa0d3c3f5609045d82f08dbb187f338
stefian <stefan.ianta@w3ai.net>
2020-06-05 13:44:22 -0300

    changed PostList and CommentList to get the data from the Query service at 4002

--- src/CommentList.js
+++ src/CommentList.js
@@ -1,18 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
 
-export default ({ postId }) => {
-  const [comments, setComments] = useState([]);
+export default ({ comments }) => {
+  // const [comments, setComments] = useState([]);
 
-  const fetchData = async () => {
-    const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
+  // const fetchData = async () => {
+  //   const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
 
-    setComments(res.data);
-  };
+  //   setComments(res.data);
+  // };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // useEffect(() => {  // to get the data at first render
+  //   fetchData();
+  // }, []);
 
   const renderedComments = comments.map(comment => {
     return <li key={comment.id}>{comment.content}</li>
