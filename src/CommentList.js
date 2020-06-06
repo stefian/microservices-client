c7b5b35cfe72fd858045e582e84e01c7adbdd7c2
stefian <stefan.ianta@w3ai.net>
2020-06-06 11:47:17 -0300

    cleanup CommentList.js

--- src/CommentList.js
+++ src/CommentList.js
@@ -1,20 +1,6 @@
 import React from 'react';
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
 
 export default ({ comments }) => {
-  // const [comments, setComments] = useState([]);
-
-  // const fetchData = async () => {
-  //   const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
-
-  //   setComments(res.data);
-  // };
-
-  // useEffect(() => {  // to get the data at first render
-  //   fetchData();
-  // }, []);
-
   const renderedComments = comments.map(comment => {
     return <li key={comment.id}>{comment.content}</li>
   });
